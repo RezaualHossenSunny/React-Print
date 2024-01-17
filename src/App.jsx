eba668b2a07ac621498bb35aead82f4743a93c11
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-01-18 01:05:50 +0600

    npm pacge

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="print">
+    <div className="Text">
+    <div className="information">
+
+    </div>
+
+</div>
+    </div>
+    </>
+  )
+}
+
+export default App
